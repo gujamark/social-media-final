d7d119b28fcc8b92e2d104151c22bb30019add5f
gujamark <guja.markozashvili.1@btu.edu.ge>
2021-07-10 12:56:12 +0400

    added error boundary

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import { Suspense } from 'react';
 import { routes } from './Routes';
 import 'rsuite/dist/styles/rsuite-dark.css';
 import Navigation from './components/navigation';
+import ErrorBoundary from './components/error-boundary';
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <Suspense fallback={<h2>Loadin Page...</h2>}>
-        <Switch>
-          {routes.map((route) => (
-            <Route
-              exact
-              path={route.path}
-              key={route.path}
-              component={route.component}
-            />
-          ))}
-        </Switch>
-      </Suspense>
-    </div>
+    <ErrorBoundary>
+      <div>
+        <Navigation />
+        <Suspense fallback={<h2>Loadin Page...</h2>}>
+          <Switch>
+            {routes.map((route) => (
+              <Route
+                exact
+                path={route.path}
+                key={route.path}
+                component={route.component}
+              />
+            ))}
+          </Switch>
+        </Suspense>
+      </div>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/components/error-boundary/ErrorBoundary.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './ErrorBoundary.module.css';
+
+class ErrorBoundary extends React.Component {
+  state = {
+    error: null,
+  };
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error,
+    });
+  }
+
+  render() {
+    const { error } = this.state;
+    const { children } = this.props;
+
+    if (error) {
+      return (
+        <div className={styles.ErrorBoundaryWrapper}>
+          <h1>Something went wrong 💥</h1>
+        </div>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
